1ef08e7dc8a6692c03c8328be7cef0015fc0fe87
Alexander Ivanov <oshli.a.er@gmail.com>
2019-08-26 15:28:45 +0300

    gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 'use strict';
 const gulp = require('gulp');
 const spawn = require('child_process').spawn;
+const del = require('del');
 
+/**
+ * Cleans build
+ */
+gulp.task('clean', function() {
+  return del('/build');
+});
+
+/**
+ * Runs clasp
+ */
 gulp.task('clasp', function(cb) {
   cb = cb || console.log;
   const cmd = spawn('./node_modules/.bin/clasp', ['push'], {
@@ -13,9 +24,34 @@ gulp.task('clasp', function(cb) {
   });
 });
 
+gulp.task('devPrep', function devPrep() {
+  return gulp.src('./settings/dev/.clasp.json').pipe(gulp.dest('./build'));
+});
+
+gulp.task('buildPrep', function buildPrep() {
+  return gulp.src('./settings/prod/.clasp.json').pipe(gulp.dest('./build'));
+});
+
+gulp.task('preBuild', function devPrep() {
+  return gulp.src('./src/**/*.{js,gs,json,html}').pipe(gulp.dest('./build'));
+});
+
+/**
+ * Dev
+ */
+gulp.task('dev', gulp.series('clean', 'devPrep', 'preBuild', 'clasp'));
+
+/**
+ * Build
+ */
+gulp.task('build', gulp.series('clean', 'buildPrep', 'preBuild', 'clasp'));
+
+/**
+ * Watcher
+ */
 gulp.task(
   'watch',
-  gulp.series('clasp', function watch() {
-    gulp.watch(['./src/**/*.{js,gs,json,html}'], gulp.series('clasp'));
+  gulp.series('dev', function watch() {
+    gulp.watch(['./src/**/*.{js,gs,json,html}'], gulp.series('dev'));
   })
 );
